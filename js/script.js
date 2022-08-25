9f9bb4fce107510bed81d9cc0af17d67bb484e10
AlexandrKruts <kruts704@gmail.com>
2022-08-25 18:00:15 +0300

    Done second task

--- js/script.js
+++ js/script.js
@@ -17,3 +17,22 @@
 // console.log(sumTwoSmallestNumbersShort(19, 5, 42, 2, 77)); // 7
 // console.log(sumTwoSmallestNumbersShort(1, 3, 2)); // 3
 // console.log(sumTwoSmallestNumbersShort(1)); // undefined и отдельный console.error("...")
+
+
+// 2
+// function createCalculator(value) {
+//   let defaultValue = value;
+
+//   return {
+//     sum: (result) => (defaultValue += result),
+//     mult: (result) => (defaultValue *= result),
+//     sub: (result) => (defaultValue -= result),
+//     div: (result) => (defaultValue /= result),
+//   };
+// }
+
+// const calc = createCalculator(10);
+// console.log(calc.sum(5)); /// 15
+// console.log(calc.mult(10)); // 150
+// console.log(calc.sub(40)); // 110
+// console.log(calc.div(10)); // 11
