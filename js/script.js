6bc0880dda6748313f24f0bc5e2f5594c5164870
AlexandrKruts <kruts704@gmail.com>
2022-08-25 18:23:30 +0300

    corrected

--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@
 
 //1
 
-// function sumTwoSmallestNumbersShort(...array) {
-//   const sortedArray = array.sort((a, b) => a - b);
-//   let sum = sortedArray[0] + sortedArray[1];
+function sumTwoSmallestNumbersShort(...array) {
+ if (array.length <2) {
+    console.error("Введите минимум 2 числа");
+  }
+ 
+  const sortedArray = array.sort((a, b) => a - b);
+  let sum = sortedArray[0] + sortedArray[1];
+  return +sum;
 
-//   if (sortedArray.length <2) {
-//     console.error("Введите минимум 2 числа");
-//   }
-//   else{
-//     return sum;
-//   }
-// }
+}
 
-// console.log(sumTwoSmallestNumbersShort(19, 5, 42, 2, 77)); // 7
-// console.log(sumTwoSmallestNumbersShort(1, 3, 2)); // 3
-// console.log(sumTwoSmallestNumbersShort(1)); // undefined и отдельный console.error("...")
+console.log(sumTwoSmallestNumbersShort(19, 5, 42, 2, 77)); // 7
+console.log(sumTwoSmallestNumbersShort(1, 3, 2)); // 3
+console.log(sumTwoSmallestNumbersShort(1)); // undefined и отдельный console.error("...")
 
 
 // 2
